ba77f55fb832c603b9cf52208f6f37d55e49f335
DavidRamsbottom <david.ramsbottom@razorconsultancy.co.uk>
2022-04-27 16:30:32 +0100

    #3716 - SignMessageControllerTests

--- src/RTGS.Service/RTGS.Service.Tests/Controllers/SignMessageControllerTests/SignMessageControllerTests.cs
+++ src/RTGS.Service/RTGS.Service.Tests/Controllers/SignMessageControllerTests/SignMessageControllerTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Azure.Data.Tables;
 using Moq;
 using RTGS.IDCryptSDK.JsonSignatures;
+using RTGS.IDCryptSDK.JsonSignatures.Models;
 using RTGS.Service.Controllers;
 using RTGS.Service.Dtos;
 using RTGS.Service.Models;
@@ -20,24 +21,40 @@ public class SignMessageControllerTests
 	}
 
 	[Fact]
-	public async Task WhenPostingSignMessageRequest_ThenSignMessageIsCalledWithExpected()
+	public async Task WhenPostingSignMessageRequest_AndBankConnectionExists_ThenSignMessageIsCalledWithExpected()
 	{
+		var signMessageRequest = new SignMessageRequest
+		{
+			Alias = "alias",
+			Message = "message",
+			RtgsGlobalId = "rtgs-global-id"
+		};
+
+		var bankPartnerConnection = new BankPartnerConnection()
+		{
+			PartitionKey = "rtgs-global-id",
+			RowKey = "alias",
+			ConnectionId = "connection-id"
+		};
+
+		var signDocumentResponse = new SignDocumentResponse
+		{
+			PairwiseDidSignature = "pairwise-did-signature",
+			PublicDidSignature = "public-did-signature"
+		};
+
 		var jsonSignaturesClientMock = new Mock<IJsonSignaturesClient>();
 		var storageTableResolver = new Mock<IStorageTableResolver>();
-
 		var tableClient = new Mock<TableClient>();
 		var bankPartnerConnections = new Mock<Azure.Pageable<BankPartnerConnection>>();
 
+		jsonSignaturesClientMock
+			.Setup(client => client.SignJsonDocumentAsync(signMessageRequest.Message, bankPartnerConnection.ConnectionId, It.IsAny<CancellationToken>()))
+			.ReturnsAsync(signDocumentResponse)
+			.Verifiable();
+
 		bankPartnerConnections.Setup(bankPartnerConnections => bankPartnerConnections.GetEnumerator()).Returns(
-			new List<BankPartnerConnection>()
-			{
-				new BankPartnerConnection()
-				{
-					PartitionKey = "rtgs-global-id",
-					RowKey = "alias",
-					ConnectionId = "connection-id"
-				}
-			}.GetEnumerator());
+			new List<BankPartnerConnection>() { bankPartnerConnection }.GetEnumerator());
 
 		tableClient.Setup(tableClient =>
 			tableClient.Query<BankPartnerConnection>(It.IsAny<string>(), It.IsAny<int?>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
@@ -50,14 +67,9 @@ public class SignMessageControllerTests
 		var controller = new SignMessageController(
 			storageTableResolver.Object,
 			jsonSignaturesClientMock.Object);
-
-		var signMessageRequest = new SignMessageRequest
-		{
-			Alias = "alias",
-			Message = "message",
-			RtgsGlobalId = "rtgs-global-id"
-		};
-
+		
 		await controller.Post(signMessageRequest);
+
+		jsonSignaturesClientMock.Verify();
 	}
 }
