464851a8fcb03ac0d4d3437f74d9accaf475faeb
Joe Reilly <joseph.reilly@rtgs.com>
2022-05-06 13:15:43 +0100

    ConnectionController WIP

---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Tests/Controllers/ConnectionControllerTests/GivenCreateConnectionInvitationRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RTGS.IDCrypt.Service.Controllers;
+using RTGS.IDCryptSDK.Connections;
+using Xunit;
+
+namespace RTGS.IDCrypt.Service.Tests.Controllers.ConnectionControllerTests;
+
+public class GivenCreateConnectionInvitationRequest
+{
+	private readonly Mock<IConnectionsClient> _connectionsClientMock;
+
+	public GivenCreateConnectionInvitationRequest()
+	{
+		_connectionsClientMock = new Mock<IConnectionsClient>();
+	}
+
+	[Fact]
+	public void WhenPosting_ThenReturnOkResponse()
+	{
+		var connectionController = new ConnectionController();
+
+		var response = connectionController.Post();
+
+		response.Should().BeOfType<OkResult>();
+	}
+
+	[Fact]
+	public void WhenPosting_ThenCallCreateInvitationAsyncWithExpected() =>
+		_connectionsClientMock.Verify(connectionsClient =>
+			connectionsClient.CreateInvitationAsync(
+				It.IsAny<string>(),
+				true,
+				false,
+				false,
+				It.IsAny<CancellationToken>()),
+			Times.Once);
+}
---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service/Controllers/ConnectionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RTGS.IDCryptSDK.Connections;
+
+namespace RTGS.IDCrypt.Service.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ConnectionController : ControllerBase
+	{
+		private readonly IConnectionsClient _connectionsClient;
+
+		public ConnectionController(IConnectionsClient connectionsClient)
+		{
+			_connectionsClient = connectionsClient;
+		}
+
+		[HttpPost]
+		public IActionResult Post()
+		{
+			_connectionsClient.CreateInvitationAsync(
+				createConnectionInvitationRequest.Alias,
+				createConnectionInvitationRequest.AutoAccept,
+				createConnectionInvitationRequest.MultiUse,
+				createConnectionInvitationRequest.UsePublicDid);
+
+			return Ok();
+		}
+	}
+}
