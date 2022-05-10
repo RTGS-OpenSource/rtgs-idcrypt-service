f541a1cb9f6cdc4c3fd16d7905fa86c07f736f46
Darren <darren.osborne@rtgs.com>
2022-05-10 10:45:44 +0100

    xmldoc typos and terminology

--- src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureRequest.cs
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureRequest.cs
@@ -1,24 +1,24 @@
 ﻿namespace RTGS.IDCrypt.Service.Contracts.VerifyMessage;
 
 /// <summary>
-/// Represents the data required to verity a signature
+/// Represents the data required to verify a signature
 /// </summary>
 public record VerifyPrivateSignatureRequest
 {
 	/// <summary>
-	/// The RTGS.global id of the connection with which to sign the document 
+	/// The RTGS.global Id of the connection with which to sign the document 
 	/// </summary>
 	public string RtgsGlobalId { get; init; }
 	/// <summary>
-	/// The private message to verify.
+	/// The message to verify.
 	/// </summary>
 	public string Message { get; init; }
 	/// <summary>
-	/// The private signature of the signed document.
+	/// The private signature of the signed message.
 	/// </summary>
 	public string PrivateSignature { get; init; }
 	/// <summary>
-	/// The alias of the connection with which to sign the document
+	/// The alias of the connection with which to sign the message
 	/// </summary>
 	public string Alias { get; init; }
 }
