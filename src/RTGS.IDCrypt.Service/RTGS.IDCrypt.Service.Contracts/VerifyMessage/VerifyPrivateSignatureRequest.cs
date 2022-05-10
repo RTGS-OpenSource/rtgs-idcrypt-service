7d77f91afa8a81e427169d8d1df9a0dc0e84ffef
Darren <darren.osborne@rtgs.com>
2022-05-10 10:55:39 +0100

    added full stops

--- src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureRequest.cs
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureRequest.cs
@@ -1,12 +1,12 @@
 ﻿namespace RTGS.IDCrypt.Service.Contracts.VerifyMessage;
 
 /// <summary>
-/// Represents the data required to verify a signature
+/// Represents the data required to verify a signature.
 /// </summary>
 public record VerifyPrivateSignatureRequest
 {
 	/// <summary>
-	/// The RTGS.global Id of the connection with which to sign the document 
+	/// The RTGS.global Id of the connection with which to sign the document.
 	/// </summary>
 	public string RtgsGlobalId { get; init; }
 	/// <summary>
@@ -18,7 +18,7 @@ public record VerifyPrivateSignatureRequest
 	/// </summary>
 	public string PrivateSignature { get; init; }
 	/// <summary>
-	/// The alias of the connection with which to sign the message
+	/// The alias of the connection with which to sign the message.
 	/// </summary>
 	public string Alias { get; init; }
 }
--- src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureResponse.cs
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Contracts/VerifyMessage/VerifyPrivateSignatureResponse.cs
@@ -1,12 +1,12 @@
 ﻿namespace RTGS.IDCrypt.Service.Contracts.VerifyMessage;
 
 /// <summary>
-/// Represents the response to a <see cref="VerifyPrivateSignatureRequest"/>
+/// Represents the response to a <see cref="VerifyPrivateSignatureRequest"/>.
 /// </summary>
 public record VerifyPrivateSignatureResponse
 {
 	/// <summary>
-	/// True if the signature is verified, otherwise false
+	/// True if the signature is verified, otherwise false.
 	/// </summary>
 	public bool Verified { get; init; }
 }
