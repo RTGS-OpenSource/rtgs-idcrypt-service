13ac2e601e5c7aed39fc1da43c72dcd20ae5460d
Matt Lethargic <mattlethargic@gmail.com>
2022-06-14 11:15:48 +0100

    Adding function app

---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Functions/BankConnectionCycle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace RTGS.IDCrypt.Service.Functions;
+
+public class BankConnectionCycle
+{
+	private readonly ILogger<BankConnectionCycle> _logger;
+	private readonly HttpClient _httpClient;
+
+	public BankConnectionCycle(ILogger<BankConnectionCycle> logger, HttpClient httpClient)
+	{
+		_logger = logger;
+		_httpClient = httpClient;
+	}
+	
+	[Function(nameof(BankConnectionCycle))]
+	public async Task RunAsync([TimerTrigger("%ConnectionCycleTriggerTime%")] TimerInfo timerInfo, ILogger log)
+	{
+		_logger.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+
+		try
+		{
+			var partnerIds = await _httpClient.GetFromJsonAsync<string[]>("api/connection/InvitedPartnerIds");
+			
+			
+			// call POST to service
+		}
+		catch (Exception ex)
+		{
+			_logger.LogError(ex, "Unable to update accounts from {RequestType}", nameof(BankConnectionCycle));
+			throw;
+		}
+	}
+}
---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RTGS.IDCrypt.Service.Functions;
+
+public class Program
+{
+	public static void Main()
+	{
+		var host = new HostBuilder()
+			.ConfigureAppConfiguration(configurationBuilder => configurationBuilder
+				.AddJsonFile("local.settings.json", true, false))
+			.ConfigureFunctionsWorkerDefaults()
+			.ConfigureServices((context, services) =>
+			{
+				services.AddHttpClient<BankConnectionCycle>(cfg =>
+				{
+					cfg.BaseAddress = new Uri(context.Configuration.GetValue<string>("IdCryptServiceBaseAddress"));
+				});
+				services.AddSingleton<ITelemetryInitializer>(new TelemetryInitalizer());
+				services.AddApplicationInsightsTelemetryWorkerService();
+			})
+			.Build();
+		
+		host.Run();
+	}
+}
