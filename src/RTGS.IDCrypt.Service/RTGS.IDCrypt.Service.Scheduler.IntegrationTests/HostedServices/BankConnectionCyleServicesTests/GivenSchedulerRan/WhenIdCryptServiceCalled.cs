c9898dc87f226a592644e3643220dc47f3959b1d
Matt Lethargic <mattlethargic@gmail.com>
2022-06-16 14:44:56 +0100

    Adding integration tests

---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.Scheduler.IntegrationTests/HostedServices/BankConnectionCyleServicesTests/GivenSchedulerRan/WhenIdCryptServiceCalled.cs
@@ -0,0 +1,11 @@
+﻿namespace RTGS.IDCrypt.Service.Schedulr.IntegrationTests.HostedServices.BankConnectionCyleServicesTests.GivenSchedulerRan;
+
+public class WhenIdCryptServiceCalled
+{
+
+
+	public void ThenBaseAddressSet()
+	{
+		
+	}
+}
