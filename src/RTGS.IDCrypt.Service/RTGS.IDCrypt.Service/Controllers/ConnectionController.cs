04ef4b74093f4c2b54dee563e9c9bf000f13690a
Darren <darren.osborne@rtgs.com>
2022-06-21 12:08:23 +0100

    should be perm redirect

--- src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service/Controllers/ConnectionController.cs
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service/Controllers/ConnectionController.cs
@@ -11,26 +11,26 @@ public class ConnectionController : ControllerBase
 {
 	[HttpPost("cycle")]
 	public IActionResult Cycle(CycleConnectionRequest request) =>
-		RedirectPreserveMethod("/api/bank-connection/cycle");
+		RedirectPermanentPreserveMethod("/api/bank-connection/cycle");
 
 	[HttpPost("for-bank")]
 	public IActionResult ForBank(CreateConnectionInvitationForBankRequest request, CancellationToken cancellationToken = default) =>
-		RedirectPreserveMethod("/api/bank-connection/create");
+		RedirectPermanentPreserveMethod("/api/bank-connection/create");
 
 	[HttpPost("for-rtgs")]
 	public IActionResult ForRtgs(CancellationToken cancellationToken = default) =>
-		RedirectPreserveMethod("/api/rtgs-connection/create");
+		RedirectPermanentPreserveMethod("/api/rtgs-connection/create");
 
 	[HttpPost("accept")]
 	public ActionResult Accept(
 		AcceptConnectionInvitationRequest request, CancellationToken cancellationToken = default) =>
-		RedirectPreserveMethod("a/pi/bank-connection/accept");
+		RedirectPermanentPreserveMethod("a/pi/bank-connection/accept");
 
 	[HttpDelete("{connectionId}")]
 	public IActionResult Delete(string connectionId, CancellationToken cancellationToken = default) =>
-		RedirectPreserveMethod($"/api/bank-connection/{connectionId}");
+		RedirectPermanentPreserveMethod($"/api/bank-connection/{connectionId}");
 
 	[HttpGet("InvitedPartnerIds")]
 	public ActionResult InvitedPartnerIds(CancellationToken cancellationToken = default) =>
-		RedirectPreserveMethod("/api/bank-connection/InvitedPartnerIds");
+		RedirectPermanentPreserveMethod("/api/bank-connection/InvitedPartnerIds");
 }
