104a9588cba18f5b3b6e219df668f2f511bdee14
DavidRamsbottom <david.ramsbottom@razorconsultancy.co.uk>
2022-06-22 16:17:03 +0100

    #3792 - Add Connection Cleanup Scheduler

---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.ConnectionCleanupScheduler.IntegrationTests/BankConnectionCleanupServiceTests/GivenSchedulerRan/WhenIdCryptServiceAddressSet.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using RTGS.IDCrypt.Service.ConnectionCleanupScheduler.IntegrationTests.Fixtures;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace RTGS.IDCrypt.Service.ConnectionCleanupScheduler.IntegrationTests.BankConnectionCleanupServiceTests.GivenSchedulerRan;
+
+public class WhenIdCryptServiceAddressSet : IClassFixture<TestFixture>
+{
+	private readonly TestFixture _testFixture;
+
+	public WhenIdCryptServiceAddressSet(TestFixture testFixture)
+	{
+		_testFixture = testFixture;
+	}
+
+	[Fact]
+	public async Task ThenBaseAddressSet()
+	{
+		_testFixture.Server
+			.Given(Request.Create().WithPath("/api/bank-connection/ObsoleteConnectionIds"))
+			.RespondWith(
+				Response.Create()
+					.WithStatusCode(200)
+					.WithBody("[\"connection-id-1\",\"connection-id-2\"]")
+			);
+
+		_testFixture.Server
+			.Given(Request.Create().WithPath("/api/bank-connection/connection-id-*"))
+			.RespondWith(Response.Create().WithStatusCode(200));
+
+		Environment.SetEnvironmentVariable("IdCryptServiceAddress", TestFixture.Url);
+
+		var exitCode = await TestFixture.RunProgramAsync();
+		exitCode.Should().Be(0, "exit code should be 0, if not something went wrong");
+	}
+}
---
+++ src/RTGS.IDCrypt.Service/RTGS.IDCrypt.Service.ConnectionCleanupScheduler/BankConnectionCleanupException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace RTGS.IDCrypt.Service.ConnectionCleanupScheduler;
+
+[Serializable]
+public class BankConnectionCleanupException : Exception
+{
+	public BankConnectionCleanupException()
+	{
+	}
+
+	public BankConnectionCleanupException(string message) : base(message)
+	{
+	}
+
+	public BankConnectionCleanupException(string message, Exception innerException) : base(message, innerException)
+	{
+	}
+
+	protected BankConnectionCleanupException(SerializationInfo info, StreamingContext context)
+		: base(info, context)
+	{
+	}
+}
